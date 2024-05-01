6400287eecea7e1d13f6788d0e3582f582063194
annaritadituri <annaritadituri@icloud.com>
2024-05-01 17:03:33 +0200

    Milestone 1 completed

--- js/main.js
+++ js/main.js
@@ -2,3 +2,46 @@
 
 //////////////////////////////////////////////////////////
 
+const formGeneraDati = document.getElementById('formGeneraDati');
+console.dir(formGeneraDati);
+
+formGeneraDati.addEventListener('submit', 
+    
+    function(e) {
+        e.preventDefault();
+
+        //ACQUISIRE I DATI CHE CI SERVONO IN INPUT DALL'UTENTE
+        let nameInput = document.getElementById('name').value;
+        let kmInput = document.getElementById('kilometers').value;
+        let ageInput = document.getElementById('age').value;
+
+        console.log({nameInput});
+        console.log({ageInput});
+        console.log({kmInput});
+
+        //RENDERE I DATI UTILIZZABILI
+        kmInput = Number(kmInput);
+
+        //CALCOLARE IL TOTALE DEL BIGLIETTO
+        const price = 0.21;
+        let totalPrice = (kmInput * price);
+        console.log({totalPrice});
+
+        //APPLICARE GLI SCONTI
+        let discount18 = 20;
+        //over 65
+        let discount65 = 40;
+
+        if (ageInput === "minors") {
+            totalPrice = (totalPrice - (totalPrice / 100 * discount18));
+            console.log("sono entrato nel ciclo 18;")
+        } else if (ageInput === "elderly") {
+            totalPrice = (totalPrice - (totalPrice / 100 * discount65));
+            console.log("sono entrato nel ciclo 65;")
+        }
+
+        //METTIAMO IL RISULTATO NELLA GIUSTA FORMA E LO FACCIAMO VISUALIZZARE
+        totalPrice = totalPrice.toFixed(2);
+        console.log({totalPrice});
+    }
+)
